76c92a099e4b1590d0b4c657c1e84e55e211d6a4
A22N <83749847+A22N@users.noreply.github.com>
2024-09-06 23:12:43 +0700

    test5

--- src/Website/admin/connect.php
+++ src/Website/admin/connect.php
@@ -1,17 +1,16 @@
 <?php
 
-	$dsn = 'mysql:host=localhost;dbname=shop';
-	$user = 'root';
-	$pass = '';
-	$option = array(
-		PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
-	);
+    $dsn = 'mysql:host=DB;dbname=' . getenv('MYSQL_DATABASE'); // Sử dụng tên dịch vụ MySQL từ docker-compose
+    $user = getenv('MYSQL_USER'); // Tên người dùng MySQL
+    $pass = getenv('MYSQL_PASSWORD'); // Mật khẩu MySQL
+    $option = array(
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+    );
 
-	try {
-		$con = new PDO($dsn, $user, $pass, $option);
-		$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	}
-
-	catch(PDOException $e) {
-		echo 'Failed To Connect' . $e->getMessage();
-	}
+    try {
+        $con = new PDO($dsn, $user, $pass, $option);
+        $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $e) {
+        echo 'Failed To Connect: ' . $e->getMessage();
+    }
+?>
